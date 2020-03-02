897426f16cbd8e0fb7ca540d7a74286e9a5abed7
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-03-03 00:44:18 +0200

    AddItemBar tests started

--- src/components/AddItemBar.test.js
+++ src/components/AddItemBar.test.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { v4 as uuidv4 } from "uuid";
+import { shallow, mount } from "enzyme";
+import { spy } from "sinon";
+import App from "../App";
 import AddItemBar from "./AddItemBar";
+import TodoItem from "./TodoItem";
 
-describe("<AddItemBar/>", () => {
+describe("<AddItemBar/> UI tests", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<AddItemBar />);
+  });
+
   it("should render without problems", () => {
-    shallow(<AddItemBar />);
+    expect(wrapper).not.toBeNull();
   });
 
   it("should have an input box", () => {
-    const wrapper = shallow(<AddItemBar />);
     expect(wrapper.find("input.rounded").length).toEqual(1);
   });
 
   it("should have a submit button", () => {
-    const wrapper = shallow(<AddItemBar />);
     expect(wrapper.find("button.btn-success").length).toEqual(1);
   });
+
+  it("should have a submit button text", () => {
+    expect(wrapper.find("button.btn-success").text()).toEqual("Submit");
+  });
 });
+
+// describe("<AddItemBar/> event tests", () => {
+//   it("should submit a new todo on click", () => {
+//     const wrapper = mount(<App />);
+//     const itemBar = wrapper.find(AddItemBar);
+
+//   });
+// });
