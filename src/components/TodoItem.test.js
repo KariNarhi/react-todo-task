9c414f1ddd3813b3ed2fa0a39e16b0092133e797
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-03-03 00:20:41 +0200

    problems solved (for now)

--- src/components/TodoItem.test.js
+++ src/components/TodoItem.test.js
@@ -1,37 +1,84 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 import { v4 as uuidv4 } from "uuid";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
+import { spy } from "sinon";
+import App from "../App";
 
-describe("<TodoItem />", () => {
+describe("<TodoItem /> UI tests", () => {
+  let todo, todos, wrapper, funcSpy;
+
+  beforeEach(() => {
+    todo = { id: uuidv4(), name: "Learn React", complete: false };
+    todos = [
+      { id: uuidv4(), name: "Learn React", complete: false },
+      { id: uuidv4(), name: "Graduate from school", complete: false }
+    ];
+    funcSpy = spy();
+    wrapper = shallow(
+      <TodoItem todo={todo} todos={todos} setTodos={funcSpy} />
+    );
+  });
+
   it("should render without problems", () => {
-    const todo = { id: uuidv4(), name: "Learn React", complete: false };
-    shallow(<TodoItem todo={todo} />);
+    expect(wrapper).not.toBeNull();
   });
 
   it("shallow wrapper instance should be null", () => {
-    const todo = { id: uuidv4(), name: "Learn React", complete: false };
-    const wrapper = shallow(<TodoItem todo={todo} />);
     const instance = wrapper.instance();
-
     expect(instance).toEqual(null);
   });
 
   it("should have todo text", () => {
-    const todo = { id: uuidv4(), name: "Learn React", complete: false };
-    const wrapper = shallow(<TodoItem todo={todo} />);
     expect(wrapper.find("h5").length).toEqual(1);
   });
 
   it("should have a complete button", () => {
-    const todo = { id: uuidv4(), name: "Learn React", complete: false };
-    const wrapper = shallow(<TodoItem todo={todo} />);
     expect(wrapper.find("button.btn-info").length).toEqual(1);
   });
 
   it("should have a delete button", () => {
-    const todo = { id: uuidv4(), name: "Learn React", complete: false };
-    const wrapper = shallow(<TodoItem todo={todo} />);
     expect(wrapper.find("button.btn-danger").length).toEqual(1);
+  });
+});
+
+describe("<TodoItem /> click event tests", () => {
+  it("should change complete status when clicked", () => {
+    const wrapper = mount(<App />);
+    const item = wrapper.find(TodoItem);
+
+    // from Incomplete to Complete
+    item
+      .find("button.btn-info")
+      .at(0)
+      .simulate("click");
+    expect(
+      wrapper
+        .find("button.btn-info")
+        .at(0)
+        .text()
+    ).toEqual("Complete");
+
+    // from Complete to Incomplete
+    item
+      .find("button.btn-info")
+      .at(0)
+      .simulate("click");
+    expect(
+      wrapper
+        .find("button.btn-info")
+        .at(0)
+        .text()
+    ).toEqual("Incomplete");
+  });
+
+  it("should delete item 'Learn React' when clicked", () => {
+    const wrapper = mount(<App />);
+    const item = wrapper.find(TodoItem);
+    item
+      .find("button.btn-danger")
+      .at(0) // 0 index which is the first item AKA "Learn React"
+      .simulate("click");
+    expect(wrapper.contains([<h5>Learn React</h5>])).toBeFalsy();
   });
 });
